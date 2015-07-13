16400d559396e3787ac0b6eb1f21bdfee5c95a6d
Ovan Crone <ovan@psibr.net>
2015-07-13 16:57:37 -0500

    missed interface

--- src/Eddn.Listener/IEddnListener.cs
+++ src/Eddn.Listener/IEddnListener.cs
@@ -14,7 +14,8 @@ namespace Eddn.Listener
         /// </summary>
         /// <param name="callback">The callback to execute when a message is received.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
+        /// <param name="cancellationAndTimeOutSeconds">The cancellation and time out seconds.</param>
         /// <returns>The management thread as a task.</returns>
-        Task BeginListener(Action<string> callback, CancellationToken cancellationToken = default(CancellationToken));
+        Task BeginListener(Action<string> callback, CancellationToken cancellationToken = default(CancellationToken), int cancellationAndTimeOutSeconds = 30);
     }
 }
