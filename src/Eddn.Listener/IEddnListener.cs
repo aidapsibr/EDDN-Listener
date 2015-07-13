0dac27468abdf0e83e1c24359537b10c522b8c2a
Ovan Crone <ovan@psibr.net>
2015-07-13 16:59:21 -0500

    wow im being lazy

--- src/Eddn.Listener/IEddnListener.cs
+++ src/Eddn.Listener/IEddnListener.cs
@@ -16,6 +16,6 @@ namespace Eddn.Listener
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <param name="cancellationAndTimeOutSeconds">The cancellation and time out seconds.</param>
         /// <returns>The management thread as a task.</returns>
-        Task BeginListener(Action<string> callback, CancellationToken cancellationToken = default(CancellationToken), int cancellationAndTimeOutSeconds = 30);
+        void BeginListener(Action<string> callback, CancellationToken cancellationToken = default(CancellationToken), int cancellationAndTimeOutSeconds = 30);
     }
 }
